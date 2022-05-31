d440edc718836116c75a24f64789d4a4e1464991
imran <imran.hasanpi@gmail.com>
2022-05-31 13:42:29 +0600

    second commit

---
+++ accessModifier.php
@@ -0,0 +1,36 @@
+<?php
+// git test comment
+include('errorReport.php');
+
+class Human {
+    private $name;
+
+    public function __construct($a) {
+        $this->name = $a;
+    }
+
+    public function show() {
+        return "Name is: " . $this->name;
+    }
+}
+
+
+class Man extends Human {
+    
+    public function get() {
+        echo "Your name: " . $this->name . "<br>";
+    }
+}
+
+// $second = new Human("John");
+// $second->show();
+
+$third = new Man('shire');
+echo $third->show();
+
+
+// $third->name = "goru";
+// $third->get();
+
+
+?>
